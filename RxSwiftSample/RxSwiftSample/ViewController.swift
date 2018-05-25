a6a7d37fc47bdb973f1c70b490ecd1b0cd24492b
htomcat <gllu--u.com@hotmail.co.jp>
2018-05-25 17:25:26 +0900

    [add] bind to tableView

--- RxSwiftSample/RxSwiftSample/ViewController.swift
+++ RxSwiftSample/RxSwiftSample/ViewController.swift
@@ -18,10 +18,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    @IBOutlet weak var tableView: UITableView!
+
     
     static func createWith(navigator: Navigator, storyboard: UIStoryboard, viewModel: ViewModel) -> ViewController {
         let vc = storyboard.instantiateViewController(ofType: ViewController.self)
@@ -30,41 +28,15 @@ class ViewController: UIViewController {
         return vc
     }
 
-    func fetchEvents(repo: String) {
-        let reponse = Observable.of(repo)
-            .map { urlString -> URL in
-                return URL(string: "https://api.github.com/repos/\(urlString)/events")!
-            }
-            .map { url -> URLRequest in
-                return URLRequest(url: url)
-            }
-            .flatMap { request in
-                return URLSession.shared.rx.response(request: request)
-            }
-            .share(replay: 1, scope: .whileConnected)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 90
+        tableView.rowHeight = UITableViewAutomaticDimension
 
-        reponse
-            .filter { reponse, _ in
-                return 200...300 ~= reponse.statusCode
-            }
-            .map { _, data -> [[String: Any]] in
-                guard
-                    let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
-                    let result = jsonObject as? [[String: Any]] else {
-                        return []
-                }
-                return result
-            }
-            .filter { object in
-                return object.count > 0
-            }
-            .map { object in
-                return object.compactMap(Event.init)
-            }
-            .subscribe(onNext: { [weak self] newEvents in
-                self?.events.accept(newEvents)
-            })
-            .disposed(by: disposeBag)
     }
+    
+    func bindUI() {
+    }
+    
 }
 

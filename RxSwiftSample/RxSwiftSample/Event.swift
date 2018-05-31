e1b12f1c7e54752c6beb6aafc5f574f2f52d1d7a
htomcat <gllu--u.com@hotmail.co.jp>
2018-05-31 09:56:46 +0900

    [modify] adapt object mapper to model

--- RxSwiftSample/RxSwiftSample/Event.swift
+++ RxSwiftSample/RxSwiftSample/Event.swift
@@ -8,17 +8,23 @@
 
 import Foundation
 import RealmSwift
+import ObjectMapper
 
-class Event: Object {
+class Event: Object, Mappable {
     @objc dynamic var id: Int64 = 0
     @objc dynamic var name: String = ""
+
+    func mapping(map: Map) {
+        id <- map["id"]
+        name <- map["name"]
+    }
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
 
     // MARK: - Meta
     override static func primaryKey() -> String? {
         return "id"
-    }
-
-    static func decode(events: [JSONObject]) -> [Event] {
-        return [Event]()
     }
 }

abf55b1fe7298c5590056d5fdec603bdc8f87b39
htomcat <gllu--u.com@hotmail.co.jp>
2018-06-07 10:01:04 +0900

    [add] Create custom cell

---
+++ RxSwiftSample/RxSwiftSample/EventCellView.swift
@@ -0,0 +1,27 @@
+//
+//  EventCellView.swift
+//  RxSwiftSample
+//
+//  Created by 浜田　智史 on 2018/06/07.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import UIKit
+
+class EventCellView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with event: Event) {
+    }
+
+}

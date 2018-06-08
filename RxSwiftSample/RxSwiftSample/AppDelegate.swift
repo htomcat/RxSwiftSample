9e2ca60fd12ff72e5bd2f5429f2962ed5f822683
htomcat <gllu--u.com@hotmail.co.jp>
2018-06-08 10:38:45 +0900

    [add] navigation in AppDelegate

--- RxSwiftSample/RxSwiftSample/AppDelegate.swift
+++ RxSwiftSample/RxSwiftSample/AppDelegate.swift
@@ -12,10 +12,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let navigator = Navigator()
+    let account = GithubAccount().default
+    let list = (username: "", slug: "")
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GithubAccount.set(key: "placeholder",
+                          secret: "placeholder")
+        let navigation = window!.rootViewController!
+        navigator.show(segue: .listEvents(account, list), sender: navigation)
         return true
     }
 
